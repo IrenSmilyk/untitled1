0f56fcb156a0f6e5cf81eb10b03c59a63dc855d4
IrenSmilyk <smilykirina@gmail.com>
2019-02-18 23:05:17 +0200

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         length = input.nextInt();
         array = new int[length][length];
         Random ran = new Random();
-        String s = null, s1 = null;
+
         if (length <= 1) {
             System.out.println("Невозможно создать матрицу такой длины!");
         } else {
@@ -23,72 +23,58 @@ public class Main {
                     array[row][col] = ran.nextInt(length + length) - length;
                 }
             }
-        }
-        for (int[] row : array) {
-            for (int col : row) {
-                System.out.print(col + "\t");
+
+            for (int[] row : array) {
+                for (int col : row) {
+                    System.out.print(col + "\t");
+                }
+                System.out.print("\n");
             }
-            System.out.print("\n");
-        }
 
-        int n, countMax = 0, countMin = 0;
-        int maxRow = 0, minRow = 0;
-        int max = 0, min = 0;
-        int indexRow = 0, index1Row = 0;
-        int index, index1 = 0;
-        //int[] c = new int[max + 1];
+            int indexRow = 0;
+            int indexCol = 0;
 
-        for (int i = 0; i < length; i++) {
-            for (n = 0; n < length - 1; n++) {
-                if (array[i][n] < array[i][n + 1]) {
-                    countMax++;
-                    if (countMax > maxRow) {
-                        maxRow = countMax;
-                        indexRow = n + 1; //запоминаем индекс последнего возрастающего элемента
+            for (int row = 0; row < length; row++) {
+                for (int col = 0; col < length; col++) {
+                    if (array[row][col] == 0) {
+                        indexRow = row;
+                        indexCol = col;
+                    } else break;
 
+                    for (int i = 0; i < indexRow; i++) {
+                        for (int j = 0; j < indexCol; j++) {
+                            array[i][j] = array[i][j + 1];
+                        }
+                        for (int j = indexCol; j < length - 1; j++) {
+                            array[i][j] = array[i][j + 1];
+
+
+                        }
+                       // --length;
                     }
+                    for (int i = indexRow; i < length - 1; i++) {
+                        for (int j = 0; j < indexCol; j++) {
+                            array[i][j] = array[i + 1][j];
+                        }
+                        for (int j = indexCol; j < array[i].length - 1; j++) {
+                            array[i][j] = array[i + 1][j + 1];
 
-                } else {
-                    countMax = 0;
-
-                }
-                if (array[i][n] > array[i][n + 1]) {
-                    countMin++;
-                    if (countMin > minRow) {
-                        minRow = countMin;
-                        index1Row = n + 1; //запоминаем индекс последнего убывающего элемента
+                        }
+                        //--length;
                     }
-                } else {
-                    countMin = 0;
                 }
-            }
-            if (maxRow > max) {
-                max = maxRow;
-                index = indexRow;
-                int[] c = new int[max + 1];
-                int l = 0;
-                for (int col = index - max; col < index + 1; col++) {
-                    c[l++] = array[i][col];
-                }
-                s = Arrays.toString(c);
-            }
-           /* if (minRow > min) {
-                min = minRow;
-                index1 = index1Row;
 
-                int[] d = new int[min + 1];
-                int l1 = 0;
-                for (int col = index1 - min; col < index1 + 1; col++) {
-                    d[l1++] = array[i][col];
+
+            }
+            System.out.println();
+            for (int[] k : array) {
+                for (int col : k) {
+                    System.out.print(col + "\t");
                 }
-                s1 = Arrays.toString(d);
-            }*/
+                System.out.print("\n");
+            }
+
         }
-
-        System.out.print("Максимальная последовательность возрастающих чисел:");
-        System.out.println(s + " " + (max + 1));
-        /*System.out.print("Максимальная последовательность убывающих чисел:");
-        System.out.println(s1 + " " + (min + 1));*/
-
     }
 }
+
